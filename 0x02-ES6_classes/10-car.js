60ff6a4b0cff1f1a14b8704757b7c245ef2c4a25
maramilod <marammilod03@gmail.com>
2024-04-25 23:18:25 +0100

    add

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,39 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set brand(brand) {
+    this._brand = brand;
+  }
+
+  set motor(motor) {
+    this._motor = motor;
+  }
+
+  set color(color) {
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    return new this.constructor[Symbol.species]();
+  }
+}
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,19 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('nup');
+    }
+    this._sqft = sqft;
+  }
+}
