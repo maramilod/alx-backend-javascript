94744f38c71c76bfcd597dcf95194cbb3f28b9ff
maramilod <marammilod03@gmail.com>
2024-04-30 15:12:06 +0100

    add

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    make(1, 'Guillaume', 'San Francisco'),
+    make(2, 'James', 'Columbia'),
+    make(5, 'Serena', 'San Francisco'),
+  ];
+}
